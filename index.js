c523d12d0b18182b3ca3b6baf25e1f6bde88d54c
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-05-30 13:11:01 +0700

    post many

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const database = require('./database');
 database.connectToServer();
 // config.connectToServer();
 
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credential: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- routes/customer.js
+++ routes/customer.js
@@ -56,9 +56,9 @@ router.post('/', async(req, res) => {
     } = req.body
 
     const data = await customer.find({}).toArray();
-    const dataFilter = data?.filter(customer => (customer.customer_id)?.slice(0,4) === String(YYDD))
-    const dataLength = data?.filter(customer => (customer.customer_id)?.slice(0,4) === String(YYDD))?.length
-    const dataLastId = dataFilter[dataLength - 1]?.customer_id
+    const dataFilter = data?.filter(customer => (customer._id)?.slice(0,4) === String(YYDD))
+    const dataLength = data?.filter(customer => (customer._id)?.slice(0,4) === String(YYDD))?.length
+    const dataLastId = dataFilter[dataLength - 1]?._id
     const id = dataLastId !== undefined ? Number(dataLastId.toString().slice(6,10)) + 1 : 1
 
     const strId = "" + id;
@@ -173,7 +173,7 @@ router.post('/', async(req, res) => {
             sumOther_channelMainContact: summary?.sumOther?.sumOther_channelMainContact || "",
           },
         },
-        history: history.map((history, index) => {
+        history: history?.map((history, index) => {
           return {
             customer_id: fixCustomerId,
             history_id: index + 1,
@@ -185,8 +185,127 @@ router.post('/', async(req, res) => {
     }
 
     try {
-        const result = await customer.insertOne(newCustomer)
-        res.status(200).json(result)
+        const result = Array.isArray(req.body) ? await customer.insertMany(req.body.map((data,index) => {
+          const fixCustomerIdMany = `${YYDD}-${padId.substring(0, padId.length - strId.length) + (Number(strId)+Number(""+index))}`
+          return {
+            _id: fixCustomerIdMany,
+            customer_id: fixCustomerIdMany,
+            customerType: Number(data?.customerType) || 0,
+            customerPriority: Number(data?.customerPriority) || 0,
+            isRecyclebin: data?.isRecyclebin || false,
+            createDate: `${String(today.getDate())} ${month} ${String(today.getFullYear())}`,
+            modifiedDate: `${String(today.getDate())} ${month} ${String(today.getFullYear())}`,
+            adminName: data?.adminName || "",
+            nik: Number(data?.nik) || 0,
+            fullName: data?.fullName || "",
+            nickName: data?.nickName || "",
+            gender: data?.gender || "",
+            birthDate: data?.birthDate || "",
+            phone: data?.phone || "",
+            email: data?.email || "",
+            job: data?.job || "",
+            tags: data?.tags || [],
+            address: {
+              province: data?.address?.province || "",
+              city: data?.address?.city || "",
+              district: data?.address?.district || "",
+              village: data?.address?.village || "",
+              detail: data?.address?.detail || "",
+              postalCode: data?.address?.postalCode || "",
+            },
+            family: data?.family?.map((family, index) => {
+              return {
+                customer_id: fixCustomerIdMany,
+                family_id: index + 1,
+                family_status: family.family_status || "",
+                family_fullName: family.family_fullName || "",
+                family_gender: family.family_gender || "",
+                family_birthDate: family.family_birthDate || "",
+                family_description: family.family_description || ""
+              }
+            }) || [],
+            more: {
+              morePersonal: {
+                pregnantAge: data?.more?.morePersonal?.pregnantAge || "",
+                religion: data?.more?.morePersonal?.religion || "",
+                hobby: data?.more?.morePersonal?.hobby || "",
+              },
+              moreMedsos: {
+                facebook: data?.more?.moreMedsos?.facebook || "",
+                twitter: data?.more?.moreMedsos?.twitter || "",
+                instagram: data?.more?.moreMedsos?.instagram || "",
+                website: data?.more?.moreMedsos?.website || "",
+              },
+              moreOther: {
+                dataSource: data?.more?.moreOther?.dataSource || "",
+                branch: data?.more?.moreOther?.branch || "",
+              },
+              moreNote: data?.more?.moreNote || [],
+              moreMedcon: data?.more?.moreMedcon?.map((medcon, index) => {
+                return {
+                  customer_id: fixCustomerIdMany,
+                  moreMedcon_id: index + 1,
+                  moreMedcon_phoneAlternative: medcon.moreMedcon_phoneAlternative || "",
+                  moreMedcon_whatsapp: medcon.moreMedcon_whatsapp || "",
+                  moreMedcon_emailAlternative: medcon.moreMedcon_emailAlternative || "",
+                }
+              }) || [],
+              moreMCU: data?.more?.moreMCU?.map((mcu, index) => {
+                return {
+                  customer_id: fixCustomerIdMany,
+                  moreMCU_id: index + 1,
+                  moreMCU_fullName: mcu.moreMCU_fullName || "",
+                  moreMCU_type: mcu.moreMCU_type || "",
+                  moreMCU_date: mcu.moreMCU_date || "",
+                  moreMCU_location: mcu.moreMCU_location || "",
+                  moreMCU_result: mcu.moreMCU_result || "",
+                  moreMCU_description: mcu.moreMCU_description || "",
+                }
+              }) || [],
+              moreProcom: data?.more?.moreProcom?.map((procom, index) => {
+                return {
+                  customer_id: fixCustomerIdMany,
+                  moreProcom_id: index + 1,
+                  moreProcom_productName: procom.moreProcom_productName || "",
+                  moreProcom_intensityUse: procom.moreProcom_intensityUse || "",
+                  moreProcom_transactionTotal: procom.moreProcom_transactionTotal || "",
+                  moreProcom_transactionDate: procom.moreProcom_transactionDate || "",
+                  moreProcom_transactionLocation: procom.moreProcom_transactionLocation || "",
+                }
+              }) || [],
+            },
+            summary: {
+              sumEvent: data?.summary?.sumEvent?.map((sumevent, index) => {
+                return {
+                  customer_id: fixCustomerIdMany,
+                  sumEvent_id: index + 1,
+                  sumEvent_name: sumevent.sumEvent_name || "",
+                  sumEvent_manager: sumevent.sumEvent_manager || "",
+                  sumEvent_date: sumevent.sumEvent_date || "",
+                  sumEvent_description: sumevent.sumEvent_description || ""
+                }
+              }) || [],
+              sumOther: {
+                sumOther_monthlyExpenses: data?.summary?.sumOther?.sumOther_monthlyExpenses || "",
+                sumOther_productFavorite: data?.summary?.sumOther?.sumOther_productFavorite || "",
+                sumOther_pointsAvailable: data?.summary?.sumOther?.sumOther_pointsAvailable || "",
+                sumOther_pointsExpired: data?.summary?.sumOther?.sumOther_pointsExpired || "",
+                sumOther_channelOrderFavorite: data?.summary?.sumOther?.sumOther_channelOrderFavorite || "",
+                sumOther_channelMainContact: data?.summary?.sumOther?.sumOther_channelMainContact || "",
+              },
+            },
+            history: data?.history?.map((history, index) => {
+              return {
+                customer_id: fixCustomerIdMany,
+                history_id: index + 1,
+                history_date: history.history_date || "",
+                history_time: history.history_time || "",
+                history_description: history.history_description || "",
+              }
+            }) || [],
+          }
+        })) : await customer.insertOne(newCustomer)
+        res.status(200).json({ result: result, body: req.body })
     } catch (error) {
         res.status(400).json({ message: error })
     }

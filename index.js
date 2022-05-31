7685eda01e1b626c096dc125d1a21f8d4de50e53
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-05-31 08:54:47 +0700

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
     credential: true,
     optionSuccessStatus: 200
 }
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
